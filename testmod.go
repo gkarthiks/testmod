4ee7c338acd4fac820dc7b4bc2d3989b06744275
gkarthiks <github.gkarthiks@gmail.com>
2019-03-21 12:17:34 -0700

    errors fix

--- testmod.go
+++ testmod.go
@@ -1,19 +1,22 @@
 package testmod
 
-import "fmt" 
+import (
+	"errors"
+	"fmt"
+)
 
 // Hi returns a friendly greeting
 func Hi(name, lang string) (string, error) {
-    switch lang {
-    case "en":
-        return fmt.Sprintf("Hi, %s!", name), nil
-    case "pt":
-        return fmt.Sprintf("Oi, %s!", name), nil
-    case "es":
-        return fmt.Sprintf("¡Hola, %s!", name), nil
-    case "fr":
-        return fmt.Sprintf("Bonjour, %s!", name), nil
-    default:
-        return "", errors.New("unknown language")
-    }
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
